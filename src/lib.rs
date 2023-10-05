a3ed61ea9324bcf1cfca7eb96a0ce4d1deec37bf
amigin <amigin@gmail.com>
2023-10-05 15:09:59 +0300

    Fixes

--- src/lib.rs
+++ src/lib.rs
@@ -4,5 +4,7 @@ mod get_client_id;
 pub mod middlewares;
 pub use api_result_status::*;
 pub use get_client_id::*;
+#[cfg(feature = "auth-middleware")]
 mod configure_rest_api_server;
+#[cfg(feature = "auth-middleware")]
 pub use configure_rest_api_server::*;
