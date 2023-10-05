9e7f08cabfe44a9b823658e51a5059e308269bf8
amigin <amigin@gmail.com>
2023-10-05 15:47:21 +0300

    Fixes

--- src/lib.rs
+++ src/lib.rs
@@ -8,3 +8,8 @@ pub use get_client_id::*;
 mod configure_rest_api_server;
 #[cfg(feature = "auth-middleware")]
 pub use configure_rest_api_server::*;
+
+#[cfg(not(feature = "auth-middleware"))]
+mod configure_rest_api_server_with_no_auth_middleware;
+#[cfg(not(feature = "auth-middleware"))]
+pub use configure_rest_api_server_with_no_auth_middleware::*;
